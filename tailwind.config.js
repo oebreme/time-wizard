9929a03cd1ab72f503b846514ba1e326b858a127
Johannes Lenk <Lenk.Johannes@gmx.de>
2024-07-18 00:26:48 +0200

    add theme changer (´▽`ʃ♡ƪ)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,18 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui"
+
 module.exports = {
   content: [
     "./src/**/*.{html,ts}",
   ],
+  daisyui: {
+    themes: ["lofi", "black"],
+  },
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    daisyui,
+  ],
 }
 
